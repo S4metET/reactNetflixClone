70e7a171d58727fd1fe07171456fdc624cc08fb3
Samet Eren Terzi <s4metet@gmail.com>
2025-03-11 21:32:04 +0300

    Sayfalar arası geçiş yapıldı

--- src/components/trending.jsx
+++ src/components/trending.jsx
@@ -1,8 +1,27 @@
+import { useState } from "react";
 import { FaCircle } from "react-icons/fa";
 import { TbMovie } from "react-icons/tb";
-import { CiBookmark } from "react-icons/ci";
+import { CiBookmark, CiBookmarkCheck } from "react-icons/ci";
 
-const Trending = () => {
+const Trending = ({ addBookmark }) => {
+    const [saved, setSaved] = useState(false);
+
+    const handleBookmarkClick = () => {
+        setSaved(!saved);
+        const movie = {
+            title: "Beyond Earth",
+            year: "2019",
+            genre: "Movie",
+            rating: "PG"
+        };
+        
+        if (!saved) {
+            addBookmark(movie);
+        } else {
+            console.log("Beyond Earth kayıttan kaldırıldı!");
+        }
+    };
+
     return (
         <div>
             <div>
@@ -22,12 +41,12 @@ const Trending = () => {
                 <div>
                     <h1>PG</h1>
                 </div>
-                <div>
-                    <CiBookmark />
+                <div onClick={handleBookmarkClick} style={{ cursor: "pointer" }}>
+                    {saved ? <CiBookmarkCheck /> : <CiBookmark />}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Trending
+export default Trending;
