0c5fddb75bfcf0c9ed9418779d159a97a4191254
Samet Eren Terzi <s4metet@gmail.com>
2025-03-11 13:25:42 +0300

    Projeye Başlandı

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,27 @@
+import { MdMovie } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
+import { TbMovie } from "react-icons/tb";
+import { GiTv } from "react-icons/gi";
+import { FaBookmark } from "react-icons/fa";
+import profileImage from "../image/user.svg"
+
+const Sidebar = () => {
+    return (
+        <header >
+            <div>
+                <a href=""><MdMovie /></a>
+            </div>
+            <div>
+                <a href=""><FaHome /></a>
+                <a href=""><TbMovie /></a>
+                <a href=""><GiTv /></a>
+                <a href=""><FaBookmark /></a>
+            </div>
+            <div>
+                <a href=""><img src={profileImage} alt="" /></a>
+            </div>
+        </header>
+    )
+}
+
+export default Sidebar
