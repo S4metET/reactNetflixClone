bc0b3fa5fd308e68154a30dd7e895057b02fe503
Abdulkadir KAHRAMAN <a.kadirkahraman555@gmail.com>
2025-03-11 14:32:53 +0300

    sidebar css yazıldı

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -2,12 +2,12 @@ import { IoSearchOutline } from "react-icons/io5";
 
 const Search = () => {
     return (
-        <div>
+        <div className="flex bg-[#000000] gap-4 items-center ">
             <div>
-            <IoSearchOutline />
-                <div>
-                    <input type="text" name="" id="" placeholder="Search for movies or TV series"/>
-                </div>
+            <IoSearchOutline className="text-white" />
+            </div>
+            <div className="text-white">   
+                <input className="text-white w-[214px]" type="text" name="" id="" placeholder="Search for movies or TV series"/>    
             </div>
         </div>
     )
