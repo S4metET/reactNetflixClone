cf886610f463fd5140898dbbcc5ee525e1db3bc8
Samet Eren Terzi <s4metet@gmail.com>
2025-03-13 22:32:12 +0300

    Arama yapıldı

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,53 @@
+import { useState, useEffect } from "react";
 import Recommended from "./recommended";
 import Trending from "./trending";
 
-export default function HomePage(){
-    return(
-        <main>
-            <div>
-                <Trending/>
-                <div>
-                <Recommended/>
-                </div>
-            </div>
-        </main>
-    )
+export default function HomePage() {
+  const [recommendedData, setRecommendedData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchRecommendedData = async () => {
+      try {
+        const response = await fetch("https://clonejson.vercel.app/api/movies?limit=10");
+        if (!response.ok) {
+          throw new Error("Error fetching recommended data");
+        }
+        const data = await response.json();
+        setRecommendedData(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchRecommendedData();
+  }, []);
+
+  const filteredRecommended = recommendedData.filter((item) =>
+    item.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <main>
+      <div>
+        <Trending />
+        <div>
+          <input
+            type="text"
+            placeholder="Arama yapın..."
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+        </div>
+
+        <div>
+          <Recommended filteredRecommended={filteredRecommended} />
+        </div>
+      </div>
+    </main>
+  );
 }
